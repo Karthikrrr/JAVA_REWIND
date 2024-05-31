b41fc40aed6fc1cb17fa3fb919777220a673f360
karthikrrr <karthikrkarthi6@gmail.com>
2024-05-31 17:34:40 +0530

    HAS-A relation

--- PizzaServe.java
+++
@@ -1,6 +0,0 @@
-public interface PizzaServe {
-    void display(String[] item, int[] prize);
-    int choose(String[] item, int[] prize);
-    int getQuantity();
-    void getTotal(int quantity, int choose, String[] item, int[] prize);
-}
